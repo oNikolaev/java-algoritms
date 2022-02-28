9b2a6eb78448417081885c8736569e760da2182a
oleg.nikolaev <oleg.nikolaev@mango.rocks>
2022-02-28 13:36:41 +0300

    Добавил класс BinarySearch.java. В нем рассмотрена базовая модель программирования, которая используется на протяжении всего курса. Первое использование дополнительной баблиотеки algs4. В классе реализован пример реализации алгоритма бинарного поиска.

---
+++ src/part_01/part_01_01/BinarySearch.java
@@ -0,0 +1,40 @@
+package part_01.part_01_01;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static int rank(int key, int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+
+        while (lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) {
+                hi = mid - 1;
+            }
+            else if (key > a[mid]) {
+                lo = mid + 1;
+            }
+            else
+                return mid;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] whiteList = In.readInts(args[0]);
+        Arrays.sort(whiteList);
+
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (rank(key, whiteList) == -1){
+                StdOut.println(key);
+            }
+        }
+    }
+}
