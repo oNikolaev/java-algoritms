80d2add9361b5e47e758aef3aff4da40280d41ad
oleg.nikolaev <oleg.nikolaev@mango.rocks>
2022-02-28 10:47:48 +0300

    Initial commit

---
+++ src/part_01/GreatCommonDivisor.java
@@ -0,0 +1,21 @@
+package part_01;
+
+import java.util.Scanner;
+
+public class GreatCommonDivisor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int p = sc.nextInt();
+        System.out.print("Введите второе число: ");
+        int q = sc.nextInt();
+        System.out.print("НОД = " + getGCD(p, q));
+    }
+
+    public static int getGCD(int p, int q) {
+        if (q == 0)
+            return p;
+        int r = p % q;
+        return getGCD(q, r);
+    }
+}
